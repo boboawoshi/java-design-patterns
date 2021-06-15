f88d1cbb419f0cdbf9f0a65b26a35d986879b08e
jiangbo <jiangbo@253.com>
2021-06-15 16:47:25 +0800

    测试单例模式

--- singleton/src/main/java/com/iluwatar/singleton/ThreadSafeDoubleCheckLocking.java
+++ singleton/src/main/java/com/iluwatar/singleton/ThreadSafeDoubleCheckLocking.java
@@ -62,17 +62,14 @@ public final class ThreadSafeDoubleCheckLocking {
     // Joshua Bloch "Effective Java, Second Edition", p. 283-284
 
     var result = instance;
-    // Check if singleton instance is initialized.
-    // If it is initialized then we can return the instance.
+    // 检查单例对象是否被初始化.
+    // 如果初始化了，我们可以返回这个实例
     if (result == null) {
-      // It is not initialized but we cannot be sure because some other thread might have
-      // initialized it in the meanwhile.
-      // So to make sure we need to lock on an object to get mutual exclusion.
+      // 如果没有初始化，但我们不能确定 因为一些其他线程在其间可能初始化了
+      // 所以为了确保，我们需要去锁定一个对象来获得互斥
       synchronized (ThreadSafeDoubleCheckLocking.class) {
-        // Again assign the instance to local variable to check if it was initialized by some
-        // other thread while current thread was blocked to enter the locked zone.
-        // If it was initialized then we can return the previously created instance
-        // just like the previous null check.
+        //再次将实例赋值给局部变量，以检查当当前线程被阻塞进入锁定区域时，它是否被其他线程初始化。
+        // 如果它已经初始化，那么我们可以返回先前创建的实例，就像先前的null检查一样。
         result = instance;
         if (result == null) {
           // The instance is still not initialized so we can safely
